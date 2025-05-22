b48fe75ed22e59a553db5f0157bebe04ef2528cb
yatu360 <yatu360@gmail.com>
2025-05-22 13:42:49 +0100

    Initial commit

---
+++ src/main/java/com/yatu360/comic_book_store_inventory/ComicBookStoreInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.yatu360.comic_book_store_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicBookStoreInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicBookStoreInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yatu360/comic_book_store_inventory/ComicBookStoreInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yatu360.comic_book_store_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicBookStoreInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
